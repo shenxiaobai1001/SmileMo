87a982ed8872f77e02ef42c970235f9d00838db9
安吉 <22435994@qq.com>
2025-12-18 18:34:10 +0800

    修复配置解析错误bug

--- Assets/Game/Barrage/Script/Normal.cs
+++ Assets/Game/Barrage/Script/Normal.cs
@@ -41,8 +41,33 @@ public class Normal : MonoBehaviour
                 if (child.gameObject.name == "Dropdown2") barrageNormalSetting.Type = child.gameObject.GetComponent<Dropdown>().options[child.gameObject.GetComponent<Dropdown>().value].text;
                 if (child.gameObject.name == "InputField1") barrageNormalSetting.Message = child.gameObject.GetComponent<InputField>().text;
                 if (child.gameObject.name == "InputField2") barrageNormalSetting.Tip = child.gameObject.GetComponent<InputField>().text;
-                if (child.gameObject.name == "InputField3") barrageNormalSetting.Count = int.Parse(child.gameObject.GetComponent<InputField>().text);
-                if (child.gameObject.name == "InputField4") barrageNormalSetting.Delay = float.Parse(child.gameObject.GetComponent<InputField>().text);
+                if (child.gameObject.name == "InputField3")
+                {
+                    string text = child.gameObject.GetComponent<InputField>().text;
+                    if (int.TryParse(text, out int value))
+                    {
+                        barrageNormalSetting.Count = value;
+                    }
+                    else
+                    {
+                        barrageNormalSetting.Count = 1;
+                        Debug.Log("解析倍率失败，使用默认值.");
+                    }
+
+                }
+                if (child.gameObject.name == "InputField4")
+                {
+                    string text = child.gameObject.GetComponent<InputField>().text;
+                    if(float.TryParse(text, out float value))
+                    {
+                        barrageNormalSetting.Delay = value;
+                    }
+                    else
+                    {
+                        barrageNormalSetting.Delay = 0;
+                        Debug.Log("解析延迟失败，使用默认值.");
+                    }
+                }
             }
         }
     }
